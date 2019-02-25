74666a9d2f02650c143285d7fd1886a6441a954f
overkoper <piotr.kopczynski7@gmail.com>
2019-02-25 17:00:22 +0100

    added some text and styling

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,6 +6,7 @@ import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { white } from 'ansi-colors';
 
 const styles = theme => ({
   root: {
@@ -13,8 +14,20 @@ const styles = theme => ({
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
+    fontWeight: 900
+    // fontWeight: theme.typography.fontWeightRegular,
   },
+  field: {
+    background: '#f8c512',
+  },
+  text: {
+    fontSize: theme.typography.pxToRem(16),
+    color: "#323232",
+  },
+  fieldYellow:{
+    background: 'linear-gradient(180deg, #f8c512 30%, #edb60d 90%)'
+  },
+
 });
 
 function SimpleExpansionPanel(props) {
@@ -22,11 +35,38 @@ function SimpleExpansionPanel(props) {
   return (
     <div className={classes.root}>
       <ExpansionPanel>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography className={classes.heading}>FAQ Question 1</Typography>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={classes.field}>
+          <Typography className={classes.heading}>Do czego służy aplikacja moveIt?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails className={classes.fieldYellow}>
+          <Typography className={classes.text}>
+            Aplikacja moveIt ma na celu pómoc Ci przy trudach przeprowadzki. Z moveIt wybierzesz najbardziej rzetelnego przewoźnika za najniższą cenę. 
+            Jako zalogowany użytkownik możesz tworzyć listy przeprowadzkowe, które trafiają na giełdę ofert. Decydujesz co chcesz przewieźć i wybierasz najbardziej odpowiedni dla Ciebie termin. Twoje oferty będą wyceniane przez przewoźników, a Ty możesz wybrać najlepszą dla Ciebie propozycję.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} className={classes.field}>
+          <Typography className={classes.heading}>Chcę się przeprowadzić. Od czego zacząć?</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+            sit amet blandit leo lobortis eget.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel className={classes.fieldYellow}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
+          <Typography className={classes.heading}>FAQ Question 3 in progress...</Typography>
+        </ExpansionPanelSummary>
+      </ExpansionPanel>
+      <ExpansionPanel className={classes.fieldYellow}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography className={classes.heading}>FAQ Question 4</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography className={classes.fieldYellow}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
@@ -34,19 +74,14 @@ function SimpleExpansionPanel(props) {
       </ExpansionPanel>
       <ExpansionPanel>
         <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography className={classes.heading}>FAQ Question 2</Typography>
+          <Typography className={classes.heading}>FAQ Question 5</Typography>
         </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
+        <ExpansionPanelDetails className={classes.fieldYellow}>
+          <Typography >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
             sit amet blandit leo lobortis eget.
           </Typography>
         </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel disabled>
-        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-          <Typography className={classes.heading}>FAQ Question 3 in progress...</Typography>
-        </ExpansionPanelSummary>
       </ExpansionPanel>
     </div>
   );
