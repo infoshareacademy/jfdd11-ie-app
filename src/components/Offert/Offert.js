f02449eba32c1f266cfc350e3291681dcc978fd1
Przemek <pzadomski@gmail.com>
2019-02-27 16:52:11 +0100

    with styles

--- src/components/Offert/Offert.js
+++ src/components/Offert/Offert.js
@@ -3,6 +3,26 @@ import React, { Component } from "react";
 import "./Offert.css";
 
 class Offert extends Component {
+
+  state = {
+    offerts: [],
+    clients: [],
+}
+
+componentDidMount() {
+fetch(process.env.PUBLIC_URL + '/data/offerts.json')
+.then(response => response.json())
+.then(data => this.setState({ 
+    offerts: Object.entries(data).map(([id, value]) => ({ id, ...value })) 
+}))
+
+fetch(process.env.PUBLIC_URL + '/data/clients.json')
+.then(response => response.json())
+.then(data => this.setState({
+    clients: data
+}))
+}
+
   render() {
     const { offertId } = this.props.match.params;
     return (
--- src/components/Offerts/Offerts.js
+++ src/components/Offerts/Offerts.js
@@ -30,7 +30,7 @@ componentDidMount() {
     render() {
         return (
             
-            <div className="Offerts"><h1>Oferty</h1>
+            <div className="Offerts"><h1 className="offert-header">Oferty</h1>
             <div>
                 <table className="offert-table">
                     <thead></thead>
@@ -39,14 +39,18 @@ componentDidMount() {
                     const client = this.state.clients[offert.clientId]
                     return (
                     <tr key={offert.id}>
-                    <td>
-                        <b>{offert.name}</b>
-                        <ul>
-                            <li>Ilość mebli: {offert.countFurniture}</li>
-                            <li>Wystawiający: {client && client.first_name} {client && client.last_name}</li>
-                            <li><Link to={`/offerts/${offert.id}`}><button>Zobacz ofertę</button></Link></li>
+                    <td className="offert-table-data">
+                        <p className="offert-title">{offert.name}</p>
+                        <ul className="offert-list">
+                            <li><b>Ilość mebli: </b>{offert.countFurniture}</li>
+                            <li><b>Wystawiający: </b>{client && client.first_name} {client && client.last_name}</li>
+                            <li><b>Termin: </b>{offert.date}</li>
+                            <li><b>Dystans: </b>{offert.distance} km</li>
+                            <li><b>Budynek z windą: </b>{offert.isElevator ? "Tak" : "Nie"}</li>
+                            <li><b>Usługa z wniesieniem: </b>{offert.bringFurnitures ? "Tak" : "Nie"}</li>
+                            <li><Link to={`/offerts/${offert.id}`}><button className="offert-button">Zobacz ofertę</button></Link></li>
+                            <li><span className="bottom-line"></span></li>
                         </ul>
-
                     </td>
                     </tr>
                 )})}
