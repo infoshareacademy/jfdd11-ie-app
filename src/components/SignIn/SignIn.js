4937440073dfa58b6f783b92e4176c7e0ce8297c
Maciej Mazur <mr.mazurmaciej@gmail.com>
2019-03-06 16:55:27 +0100

    I added new features to menu

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -29,14 +29,11 @@ class SignIn extends Component {
 
     const { signIn } = this.props.authContext;
     signIn(this.state.email, this.state.password)
-      // .then(data =>
-      //   this.setState({
-      //     error: null,
-      //     success: "Logowanie przebiegło pomyślnie"
-      //   }))
-      // .then(data => (
-      //   window.location.href = `/myAccount`
-      // ))
+      .then(data =>
+        this.setState({
+          error: null,
+          success: 1
+        }))
       .catch(error => this.setState({ error: error, success: null }));
       
   };
@@ -96,8 +93,8 @@ class SignIn extends Component {
 
             </div>
            
-          {this.state.error && <p>{this.state.error.message}</p>}
-          {this.state.success && <Redirect to="/" />}
+          {this.state.error && <p>Twoja rejestracja przebiegła niepomyślnie</p>}
+          {this.state.success && <Redirect to="/myAccount" />}
            
             {/* {this.state.error?<p className="sign-up_error">{this.state.error.message}</p>:<p className="sign-up_success">{this.state.success}</p>} */}
           </form>
