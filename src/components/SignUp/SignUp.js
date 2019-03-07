6b35388e9695edc6799cb178e7a8029687714bae
overkoper <piotr.kopczynski7@gmail.com>
2019-03-07 16:59:25 +0100

    added success popup

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,151 +1,249 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router";
+import { NavLink } from "react-router-dom";
+
 import Footer from "../Footer";
 import Header from "../Header";
-import { NavLink } from 'react-router-dom';
+
 import "./SignUp.css";
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const initialState = {
-        name: '',
-        surname: '',
-        email: '',
-        password: '',
-        company: '',
-        phone: '',
-        isCarrier: false,
-        error: null,
-        success: null,
-}
+  name: "",
+  surname: "",
+  email: "",
+  password: "",
+  company: "",
+  phone: "",
+  isCarrier: false,
+  error: null,
+  success: null
+};
 class SignUp extends Component {
-    state = initialState;
+  state = initialState;
 
-      changeRoleToUser = () =>
-          this.setState({
-              isCarrier: false
-          });
-      changeRoleToCarrier = ()=>
-        this.setState({
-            isCarrier: true
-        });
+  componentDidMount() {
+    this.setState({
+      state: initialState
+    });
+  }
 
-      handleChange = event => {
-        const fieldName = event.target.name;
-        const value =
-          event.target.type === 'checkbox'
-            ? event.target.checked
-            : event.target.value;
-    
-        this.setState({
-          [fieldName]: value,
-        });
-      };
+  changeRoleToUser = () =>
+    this.setState({
+      isCarrier: false
+    });
+  changeRoleToCarrier = () =>
+    this.setState({
+      isCarrier: true
+    });
 
-      // signUpWithGoogle (){
-      //   const provider = new firebase.auth.GoogleAuthProvider();
-      //   firebase
-      //   .auth()
-      //   .signInWithPopup(provider)
-      //   .then(function(result){
-      //     console.log(result);
-      //   })
-      // }
+  handleChange = event => {
+    const fieldName = event.target.name;
+    const value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
 
-      handleSubmit = event => {
-        event.preventDefault();
-    
+    this.setState({
+      [fieldName]: value
+    });
+  };
+
+  // signUpWithGoogle (){
+  //   const provider = new firebase.auth.GoogleAuthProvider();
+  //   firebase
+  //   .auth()
+  //   .signInWithPopup(provider)
+  //   .then(function(result){
+  //     console.log(result);
+  //   })
+  // }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        const userId = firebase.auth().currentUser.uid;
         firebase
-          .auth()
-          .createUserWithEmailAndPassword(this.state.email, this.state.password)
-          .then(() => {
-            const userId = firebase.auth().currentUser.uid;
-            firebase
-              .database()
-              .ref('users')
-              .child(userId)
-              .set({
-                name: this.state.name,
-                surname: this.state.surname,
-                company: this.state.company,
-                phone: this.state.phone,
-                isCarrier: this.state.isCarrier
+          .database()
+          .ref("users")
+          .child(userId)
+          .set({
+            name: this.state.name,
+            surname: this.state.surname,
+            company: this.state.company,
+            phone: this.state.phone,
+            isCarrier: this.state.isCarrier
+          });
+        this.setState({
+          ...initialState,
+          error: null,
+          success: "Konto zostało założone"
+        });
+      })
+      .catch(error => this.setState({ error: error, success: null }));
+  };
 
-              });
-            this.setState({ ...initialState, error: null, success: 'Konto zostało założone' });
-          })
-          .catch(error => this.setState({ error: error, success: null }));
-          
-      };
-
-    render(){
-      console.log(this.state.error)
-      const {name, surname, phone, isCarrier, company, email, password, error, success} = this.state;
-      const errors = {
-        invalidEmail: "Niepoprawny adres e-mail.",
-        weakPassword: "Niepoprawne hasło. Hasło powinno mieć conajmniej 6 znaków.",
-        emailAlreadyInUse: "Istnieje już konto o takim adresie e-mail.",
-        other: "Coś poszło nie tak. Spóbuj ponownie za chwilę"
-      }
-        return(
-            <div className="sign-up_root">
-            <Header/>
-            <div className="sign-up_main">
-            <div className="sign-up_welcome-message">
-            <h1>Zarejestruj się!</h1>
-            <p>Do czego chcesz użyć</p>
-            <span>moveIt</span>
+  render() {
+    const {
+      name,
+      surname,
+      phone,
+      isCarrier,
+      company,
+      email,
+      password,
+      error,
+      success
+    } = this.state;
+    const errors = {
+      invalidEmail: "Niepoprawny adres e-mail.",
+      weakPassword:
+        "Niepoprawne hasło. Hasło powinno mieć conajmniej 6 znaków.",
+      emailAlreadyInUse: "Istnieje już konto o takim adresie e-mail.",
+      other: "Coś poszło nie tak. Spóbuj ponownie za chwilę"
+    };
+    return (
+      <>
+        {success ? (
+          <>
+            <div className="sign-up_root disabled">
+              <Header />
+              <div className="sign-up_main disabled" />
+              <Footer />
             </div>
-            <form onSubmit={this.handleSubmit}>
+            <div className="sign-up_success">
+              <p>Rejestracja powiodła się!</p>
+              <button onClick={() => this.props.history.push("/myAccount")}>
+                OK
+              </button>
+            </div>
+          </>
+        ) : (
+          <div className="sign-up_root">
+            <Header />
+
+            <div className="sign-up_main">
+              <div className="sign-up_welcome-message">
+                <h1>Zarejestruj się!</h1>
+                <p>Do czego chcesz użyć</p>
+                <span>moveIt</span>
+              </div>
+              <p className="sign-up_pick-role">Wybierz swoją rolę: *</p>
+              <form onSubmit={this.handleSubmit}>
                 <div className="sign-up_checkboxes">
+                
+                  <input
+                    id="user"
+                    name="user-type"
+                    type="radio"
+                    onChange={this.changeRoleToUser}
+                    required
                     
-                    <input id="user" name="user-type" type="radio" onChange={this.changeRoleToUser} required></input>
-                    <label htmlFor="user">Chcę się przeprowadzić</label>
-                 
-                    <input id="carrier" name="user-type" type="radio" onChange={this.changeRoleToCarrier}></input>
-                    <label htmlFor="carrier">Chcę pomóc innym w przeprowadzce</label>
+                  />
+                  <label htmlFor="user">Chcę się przeprowadzić</label>
+
+                  <input
+                    id="carrier"
+                    name="user-type"
+                    type="radio"
+                    onChange={this.changeRoleToCarrier}
+                  />
+                  <label htmlFor="carrier">
+                    Chcę pomóc innym w przeprowadzce
+                  </label>
                 </div>
-                <input id="name" name="name" type="text" onChange={this.handleChange} placeholder="Imię" value={name} required></input>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  onChange={this.handleChange}
+                  placeholder="Imię *"
+                  value={name}
+                  required
+                />
 
-                <input id="surname" name="surname" type="text" onChange={this.handleChange} placeholder="Nazwisko" value={surname} required></input>
+                <input
+                  id="surname"
+                  name="surname"
+                  type="text"
+                  onChange={this.handleChange}
+                  placeholder="Nazwisko *"
+                  value={surname}
+                  required
+                />
 
-                <input id="phone" name="phone" type="text" onChange={this.handleChange} placeholder="Telefon" value={phone} required></input>
+                <input
+                  id="phone"
+                  name="phone"
+                  type="text"
+                  onChange={this.handleChange}
+                  placeholder="Telefon *"
+                  value={phone}
+                  required
+                />
 
-                {isCarrier?
-                <>
-                <input id="company" name="company" type="text" onChange={this.handleChange} placeholder="Nazwa firmy" value={company}></input>
-                </>
-                :
-                null}
+                {isCarrier ? (
+                  <>
+                    <input
+                      id="company"
+                      name="company"
+                      type="text"
+                      onChange={this.handleChange}
+                      placeholder="Nazwa firmy"
+                      value={company}
+                    />
+                  </>
+                ) : null}
 
-                <input id="email" name="email" type="email" onChange={this.handleChange}placeholder="E-mail" value={email}></input>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={this.handleChange}
+                  placeholder="E-mail *"
+                  value={email}
+                />
 
-                <input type="password" id="password" name="password" onChange={this.handleChange}placeholder="Hasło" value={password}></input>
-
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={this.handleChange}
+                  placeholder="Hasło *"
+                  value={password}
+                />
+                <p style={{ color: "white", fontSize: 12 }}>
+                  * - pola obowiązkowe
+                </p>
                 <button>ZAREJESTRUJ SIĘ</button>
                 {/* <button onClick={this.signUpWithGoogle}>Zarejestruj się z Google</button> */}
-            </form>
-            <div className="sign-up_sign-in">
-            <p>Mam już konto. <NavLink to="/signin">Zaloguj mnie!</NavLink></p>
-            </div>
-            {error?
-            <p className="sign-up_error">
-            {error.code === "auth/invalid-email"?
-            errors.invalidEmail
-            :
-            error.code==="auth/weak-password"?
-            errors.weakPassword
-            :
-            error.code==="auth/email-already-in-use"?
-            errors.emailAlreadyInUse
-            :
-            errors.other}
-          </p>
-            :
-            <p className="sign-up_success">{success}</p>}
+              </form>
+              <div className="sign-up_sign-in">
+                <p>
+                  Mam już konto. <NavLink to="/signin">Zaloguj mnie!</NavLink>
+                </p>
+              </div>
+              {error ? (
+                <p className="sign-up_error">
+                  {error.code === "auth/invalid-email"
+                    ? errors.invalidEmail
+                    : error.code === "auth/weak-password"
+                    ? errors.weakPassword
+                    : error.code === "auth/email-already-in-use"
+                    ? errors.emailAlreadyInUse
+                    : errors.other}
+                </p>
+              ) : null}
             </div>
             <Footer />
-            </div>
-
-        )
-    }
+          </div>
+        )}
+      </>
+    );
+  }
 }
 export default SignUp;
