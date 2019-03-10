1b298dbf7f2ef216692a0577f35829873b105a20
overkoper <piotr.kopczynski7@gmail.com>
2019-03-10 15:19:50 +0100

    added comment key to auctions.json, added table of contents with all auctions

--- src/components/MyAuctions/MyAuctions.js
+++ src/components/MyAuctions/MyAuctions.js
@@ -1,48 +1,119 @@
-import React, {Component} from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
-import firebase from 'firebase';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Header from "../Header";
+import Footer from "../Footer";
+import firebase from "firebase";
 import { withAuth } from "../../contexts/AuthContext";
-import "./MyAuctions.css"
+import "./MyAuctions.css";
 
-
-class MyAuctions extends Component{
-  
-  state ={
-    auctions: null,
-    userId: null
-  }
+class MyAuctions extends Component {
+  state = {
+    auctions: [],
+    userId: null,
+    offers: [],
+    users: []
+  };
   componentDidMount() {
-    
     firebase
-    .database()
-    .ref("auctions")
-    .once("value")
-    .then(snapshot => snapshot.val())
-    .then(data =>this.setState({
-      userId: this.props.authContext.user.uid,
-      auctions: Object.entries(data).map(([id, value]) => ({
-        auctionId: id,
-        ...value
-      })),
-      offers: Object.entries(data).map(([id, value]) => ({
-        auctionId: id,
-        ...value
-      })).map(auction => Object.values(auction.offers)).flat()
-    })
-    
-  )
-    
+      .database()
+      .ref("auctions")
+      .once("value")
+      .then(snapshot => snapshot.val())
+      .then(data =>
+        this.setState({
+          userId: this.props.authContext.user.uid,
+          auctions: Object.entries(data).map(([id, value]) => ({
+            auctionId: id,
+            ...value
+          })),
+          offers: Object.entries(data)
+            .map(([id, value]) => ({
+              auctionId: id,
+              ...value
+            }))
+            .map(auction => Object.values(auction.offers))
+            .flat()
+        })
+      );
+    firebase
+      .database()
+      .ref("users")
+      .once("value")
+      .then(snapshot => snapshot.val())
+      .then(data =>
+        this.setState({
+          users: Object.entries(data).map(([id, value]) => ({
+            id,
+            ...value
+          }))
+        })
+      );
   }
-render(){
-  console.log(this.state.userId)
-  console.log(this.state.auctions)
-  console.log(this.state.offers)
-    return(
-        <div className="my-auctions_root">
+  render() {
+    console.log(this.state.userId);
+    console.log(this.state.auctions);
+    console.log(this.state.offers);
+    console.log(this.state.users);
+    return (
+      <div className="my-auctions_root">
         <Header />
-        <p className="MyAuctions_offer">Twoja oferta}</p>
-        <p>ss</p>
+        <h1>Moje oferty</h1>
+        <table className="offert-table">
+          <thead />
+          <tbody>
+            {this.state.auctions.map(auction => {
+              const client = this.state.users.find(()=>this.state.users.id === auction.clientId);
+              return (
+                <tr key={auction.auctionId} className="Offerts_table">
+                  <td className="offert-table-data">
+                    <p className="offerts-title">{auction.name}</p>
+                    <ul className="offert-list">
+                      <li>
+                        <b>
+                          <span className="Offerts_list-information">
+                            {" "}
+                            {client && client.first_name}{" "}
+                            {client && client.last_name}
+                          </span>
+                        </b>
+                      </li>
+                      <li>
+                        <b>Miasto dostarczenia: </b>
+                        <span className="Offerts_list-information">{auction.deliveryAddress.city}</span>
+                      </li>
+                      <li>
+                        <b>Meble: </b>
+                        <span className="Offerts_list-information">
+                          {Object.keys(auction.furnitures).length}
+                        </span>
+                      </li>
+
+                      <li>
+                        <b>Data: </b>
+                        <span className="Offerts_list-information">
+                          {auction.dateOfRemoval}
+                        </span>
+                      </li>
+                      <li>
+                        <b>Wniesienie: </b>
+                        <span className="Offerts_list-information">
+                          {auction.bringFurnitures ? "Tak" : "Nie"}
+                        </span>
+                      </li>
+                      <li>
+                        <Link to={`/offerts/${auction.id}`}>
+                          <button className="offert-button">
+                            Zobacz ofertę
+                          </button>
+                        </Link>
+                      </li>
+                    </ul>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
         {/* 
         
         Przewoźnik licytując aukcję dodaje do offerts.json obiekt, który jest elementem tablicy. 
@@ -59,9 +130,8 @@ render(){
 
           */}
         <Footer />
-        </div>
-    )
-}
-
+      </div>
+    );
+  }
 }
 export default withAuth(MyAuctions);
