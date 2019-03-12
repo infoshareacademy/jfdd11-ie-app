817c13852c9e55ae893a501565b0d3e378e763ec
magda <magda17095@wp.pl>
2019-03-12 09:23:14 +0100

    add furniture  quantity

--- src/components/CreateAuction/FurnitureForm.js
+++ src/components/CreateAuction/FurnitureForm.js
@@ -23,36 +23,56 @@ class FurnitureForm extends Component {
 
     return (
       <>
-     <div className="FurnitureForm_title"> Dodaj mebel: </div>
+        <div className="FurnitureForm_title"> Dodaj mebel: </div>
         <form onSubmit={this.handleSubmit}>
-          <div className= "FurnitureForm_information">
-            <div className= "FurnitureForm_information-details">Mebel:  
-          <input className="FurnitureForm_input1"
-            placeholder=""
-            defaultValue={furniture.name}
-            onChange={this.handleChange("name")}
-          /></div>
-          <div className= "FurnitureForm_information-details">Szerokość: 
-          <input className='FurnitureForm_input2'
-            placeholder="cm"
-            defaultValue={furniture.width}
-            onChange={this.handleChange("width")}
-          /></div>
-          <div className= "FurnitureForm_information-details">Wysokość:  
-          <input className="FurnitureForm_input3"
-            placeholder="cm"
-            defaultValue={furniture.height}
-            onChange={this.handleChange("height")}
-          /></div>
-          <div className= "FurnitureForm_information-details">Głębokość:  
-          <input className="FurnitureForm_input4"
-            placeholder="cm"
-            defaultValue={furniture.deepness}
-            onChange={this.handleChange("deepness")}
-          /></div></div>
- <button className="FurnitureForm_button" type={"submit"}>Zapisz </button>
+          <div className="FurnitureForm_information">
+            <div className="FurnitureForm_information-details">
+              Mebel:
+              <input
+                className="FurnitureForm_input1"
+                placeholder=""
+                defaultValue={furniture.name}
+                onChange={this.handleChange("name")}
+              />
+            </div>
+            <div className="FurnitureForm_information-details">
+              Szerokość:
+              <input
+                className="FurnitureForm_input2"
+                placeholder="cm"
+                defaultValue={furniture.width}
+                onChange={this.handleChange("width")}
+              />
+            </div>
+            <div className="FurnitureForm_information-details">
+              Wysokość:
+              <input
+                className="FurnitureForm_input3"
+                placeholder="cm"
+                defaultValue={furniture.height}
+                onChange={this.handleChange("height")}
+              />
+            </div>
+            <div className="FurnitureForm_information-details">
+              Głębokość:
+              <input
+                className="FurnitureForm_input4"
+                placeholder="cm"
+                defaultValue={furniture.deepness}
+                onChange={this.handleChange("deepness")}
+              />
+            </div>
+            <div className="FurnitureForm_information-details">
+              Ilość:
+              <input
+                className="FurnitureForm_input5"
+              />
+            </div>
+          </div>
+          <button className="FurnitureForm_button" type={"submit"}>
+            Zapisz{" "}
+          </button>
         </form>
-       
       </>
     );
   }
